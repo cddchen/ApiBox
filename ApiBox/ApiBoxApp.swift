b0d4f8b6dc5e2462284c59ac7e82a32c3888cb84
cddchen <1354903898@qq.com>
2023-08-08 16:45:38 +0800

    Initial Commit

---
+++ ApiBox/ApiBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiBoxApp.swift
+//  ApiBox
+//
+//  Created by cdd on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
